bdec12ee01c32ceceabc5ee79065037e5327d8c1
hrushik98 <91076764+hrushik98@users.noreply.github.com>
2023-06-24 09:29:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 import openai
 
-openai.api_key = st.secrets['openai_key']
-UGGINGFACEHUB_API_TOKEN= st.secrets['huggingfacehub_key']
+
 
 def get_pdf_text(pdf_docs):
     text = ""

4ba8f01335fa9d4b3d155775a7a9df1e06683ec0
hrushik98 <91076764+hrushik98@users.noreply.github.com>
2023-06-24 09:27:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+import openai
 
 openai.api_key = st.secrets['openai_key']
 UGGINGFACEHUB_API_TOKEN= st.secrets['huggingfacehub_key']
